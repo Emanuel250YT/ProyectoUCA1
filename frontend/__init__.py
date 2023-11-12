0345951ce143f730dbb0354302361fc93b35cb5e
@TheoSempaii <61527540+TheoSempaii@users.noreply.github.com>
2023-11-11 23:58:19 -0300

    Añadido index.html

--- frontend/__init__.py
+++ frontend/__init__.py
@@ -1,2 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
 def startFrontend():
-    return print("a")
+    app.run(port=80, debug=True)
